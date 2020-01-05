fccd6ad56a2b9d592e9be871b41605c1f17137ec
NantVim <nantvim@gmail.com>
2020-01-05 23:22:46 +0400

    Added dialogue

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return(
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Dimyxh
+        </div>
+        <div className={s.dialog}>
+          Andrey
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+        <div className={s.dialog}>
+          Sasha
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>H!</div>
+        <div className={s.message}>How is yous</div>
+        <div className={s.message}>Yooo</div>
+      </div>
     </div>
   );
 };
